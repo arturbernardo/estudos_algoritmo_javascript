67d8b2164b61777955d68fb935f57f135cb66f4f
arturbernardo <artur.bernardo.k@gmail.com>
2024-04-21 20:53:48 -0300

    some witchcraft

--- fundamentals/clojure.js
+++ fundamentals/clojure.js
@@ -14,3 +14,7 @@ let innerCounter = myNewFunction(); // counter -> 2
 console.log(innerCounter); // innerCounter -> 2
 innerCounter = 33; // innerCounter -> 33
 myNewFunction(); // counter -> 3
+console.log(outer.counter); // undefined
+console.log(myNewFunction.counter); // undefined
+
+//counter is not accessible inside the global scope, but it's there.
